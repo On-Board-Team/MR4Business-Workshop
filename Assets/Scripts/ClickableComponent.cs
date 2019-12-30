68b993557438c3161ff7119414d0401f1af0a727
Cherif Redissi <cherifredissi@hotmail.com>
2019-12-30 11:27:20 +0100

    Input Down/Up Handling

---
+++ Assets/Scripts/ClickableComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+public class ClickableComponent : MonoBehaviour, IMixedRealityInputHandler
+{
+    Renderer _renderer;
+    public void OnInputDown(InputEventData eventData)
+    {
+        _renderer.material.color = Color.red;
+    }
+    public void OnInputUp(InputEventData eventData)
+    {
+        _renderer.material.color = Color.green;
+    }
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+}
